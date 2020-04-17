a0286400d62018085c7d5a9c5272d9584cd56c5e
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-04-17 00:22:00 -0400

    got episode component to render

--- src/components/episode/episode.jsx
+++ src/components/episode/episode.jsx
@@ -3,12 +3,20 @@ import { useParams } from "react-router";
 import axios from "axios";
 let Episode = (props) => {
 	const [video, setEpisode] = useState({});
+	const [loaded, setLoaded] = useState(false);
 	const { episodeID } = useParams();
 	useEffect(() => {
-		axios.get(`/api/episode/${episodeID}`).then((res) => {
-			console.log(res);
-		});
+		if (!loaded) {
+			axios.get(`/api/episode/?episode=${episodeID}`).then((res) => {
+				setEpisode(res.data);
+				setLoaded(true);
+			});
+		}
 	});
-	return <div>test</div>;
+	return (
+		<div>
+			<iframe src={`${video.url}`}></iframe>
+		</div>
+	);
 };
 export default Episode;
